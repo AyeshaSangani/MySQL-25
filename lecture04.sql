63185419135c4a3e46c90a11e88eaa412481451b
AyeshaSangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-01-05 02:02:46 +0500

    practice

---
+++ lecture04.sql
@@ -0,0 +1,10 @@
+      DBMS ( database management system )
+
+create new database "rough"
+  
+create table "employees"
+  
+coloumns => id, name, Designation, dept, email, phone, shift, city
+
+
+= same as before in previous lectures .............. 
